a9e80c0213aa53ea1dc83f956969ebf200a523fd
0xdbe <23268141+0xdbe@users.noreply.github.com>
2023-12-20 16:15:07 +0100

    feat: add default src in csp

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return getHeaders()
+    }
+}
+
+const contentSecurityPolicy = {
+    'default-src': [`'none'`],
+}
+
+function getHeaders() {
+    return [
+        {
+          source: '/(.*)',
+          headers: [
+            { key: 'Content-Security-Policy', value: getContentSecurityPolicy(contentSecurityPolicy)},
+            { key: 'X-Content-Type-Options', value: 'nosniff'},
+            //{ key: 'Permissions-Policy', value: "...",},
+            { key: 'Referrer-Policy', value: 'no-referrer'},
+            { key: 'X-Frame-Options',value: 'DENY'},
+            { key: 'X-XSS-Protection', value: '0'},
+  
+          ],
+        },
+    ];
+}
+
+function getContentSecurityPolicy(config) {
+    return Object.entries(config)
+    .map(([key, value]) => `${key} ${value.join(' ')}`)
+    .join('; ')
+  }
 
 module.exports = nextConfig
