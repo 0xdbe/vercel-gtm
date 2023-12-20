e46ff786f351c407a48ac247dc3edcbcfca60428
0xdbe <23268141+0xdbe@users.noreply.github.com>
2023-12-20 17:36:34 +0100

    feat: add middleware

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return getHeaders()
-    }
+    // async headers() {
+    //     return getHeaders()
+    // }
 }
 
 const contentSecurityPolicy = {
