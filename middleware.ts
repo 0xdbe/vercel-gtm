ee6a55c988f91767f8e74068d9b16fba964e461f
0xdbe <23268141+0xdbe@users.noreply.github.com>
2024-01-26 11:55:38 +0100

    fix: update csp for gtm

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://www.googletagmanager.com;
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
