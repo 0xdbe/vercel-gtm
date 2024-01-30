3fd8fbf93475de1e0552892ddcaace8af550d4c1
0xdbe <23268141+0xdbe@users.noreply.github.com>
2024-01-30 14:45:42 +0100

    fix: use getImageProps in place of Image to remove unsafe inline style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-import Image from 'next/image'
+import Image, {getImageProps } from 'next/image'
 
 export default function Home() {
+
+  const { style: _logoVercelPropsOmitStyle, ...logoVercelProps } = getImageProps({
+    src: '/vercel.svg',
+    alt: 'Vercel Logo',
+    width: 100,
+    height: 50,
+  }).props;
+
+  const { style: _logoNextPropsOmitStyle, ...logoNextProps } = getImageProps({
+    src: '/next.svg',
+    alt: 'Next.js Logo',
+    width: 180,
+    height: 37,
+  }).props;
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -16,26 +32,16 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <img {...logoVercelProps} 
+              className="dark:invert" 
             />
           </a>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <img {...logoNextProps} 
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" 
         />
       </div>
 
